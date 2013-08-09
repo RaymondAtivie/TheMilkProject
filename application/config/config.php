6f02fc3c87c39ffcb70915407bda87c06d62ccc2
Raymond Ativie <RaymondAtivie@gmail.com>
2013-08-09 11:28:23 +0100

    Lower case URL in config and HTacess file

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://raymond.dev/TheMilkProject/';
+$config['base_url']	= 'http://raymond.dev/themilkproject/';
 
 /*
 |--------------------------------------------------------------------------
